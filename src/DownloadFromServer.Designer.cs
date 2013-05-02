3fc6343b5951ad704bceb7d6e34c4b64b9ef5b12
jhitze <jedmaster@gmail.com>
2013-05-02 14:02:25 -0400

    more rearranging

--- src/DownloadFromServer.Designer.cs
+++ src/DownloadFromServer.Designer.cs
@@ -42,19 +42,19 @@
             // 
             this.textBoxURL.Location = new System.Drawing.Point(12, 61);
             this.textBoxURL.Name = "textBoxURL";
-            this.textBoxURL.Size = new System.Drawing.Size(260, 22);
+            this.textBoxURL.Size = new System.Drawing.Size(501, 22);
             this.textBoxURL.TabIndex = 0;
             // 
             // textBoxUserName
             // 
-            this.textBoxUserName.Location = new System.Drawing.Point(103, 89);
+            this.textBoxUserName.Location = new System.Drawing.Point(212, 89);
             this.textBoxUserName.Name = "textBoxUserName";
             this.textBoxUserName.Size = new System.Drawing.Size(100, 22);
             this.textBoxUserName.TabIndex = 1;
             // 
             // textBoxPassword
             // 
-            this.textBoxPassword.Location = new System.Drawing.Point(103, 117);
+            this.textBoxPassword.Location = new System.Drawing.Point(212, 117);
             this.textBoxPassword.Name = "textBoxPassword";
             this.textBoxPassword.Size = new System.Drawing.Size(100, 22);
             this.textBoxPassword.TabIndex = 2;
@@ -62,7 +62,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(130, 41);
+            this.label1.Location = new System.Drawing.Point(250, 41);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(24, 17);
             this.label1.TabIndex = 3;
@@ -71,7 +71,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(29, 92);
+            this.label2.Location = new System.Drawing.Point(135, 92);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(71, 17);
             this.label2.TabIndex = 4;
@@ -80,7 +80,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(29, 120);
+            this.label3.Location = new System.Drawing.Point(138, 120);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(68, 17);
             this.label3.TabIndex = 5;
@@ -89,7 +89,7 @@
             // 
             // buttonDownload
             // 
-            this.buttonDownload.Location = new System.Drawing.Point(103, 145);
+            this.buttonDownload.Location = new System.Drawing.Point(212, 145);
             this.buttonDownload.Name = "buttonDownload";
             this.buttonDownload.Size = new System.Drawing.Size(100, 23);
             this.buttonDownload.TabIndex = 6;
@@ -101,7 +101,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 233);
+            this.ClientSize = new System.Drawing.Size(525, 213);
             this.Controls.Add(this.buttonDownload);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
