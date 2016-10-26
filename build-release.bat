3b07fc1eded40e77f78775a04dc55dbc43d17149
Scott Lerch <scottlerch@gmail.com>
2016-10-25 18:50:36 -0700

    Strongly name release binaries.

--- build-release.bat
+++ build-release.bat
@@ -4,4 +4,4 @@ if exist logs goto msbuild
 mkdir logs
 
 :msbuild
-"%PROGRAMFILES(X86)%\MSBuild\14.0\Bin\MSBUILD.exe" HostsFileEditor.proj /t:Build /p:Configuration=Release /l:FileLogger,Microsoft.Build.Engine;logfile=logs\build-release.log
+"%PROGRAMFILES(X86)%\MSBuild\14.0\Bin\MSBUILD.exe" HostsFileEditor.proj /t:Build /p:Configuration=Release /p:Sign=true /l:FileLogger,Microsoft.Build.Engine;logfile=logs\build-release.log
